bbc71813601e1c146a717a01d5916b133af848ab
david <davidclass1@gmail.com>
2021-11-24 17:15:45 -0500

    proyecto inicial

---
+++ src/main/java/com/example/reestructure/SpringBootApirestReestructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.reestructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApirestReestructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApirestReestructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reestructure/SpringBootApirestReestructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reestructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApirestReestructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
